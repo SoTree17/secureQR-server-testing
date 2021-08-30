d20d4ffb0a80fb1d117c554b04a2d77f933cd2ad
ChoiJunho <ajchoi0928@gmail.com>
2021-08-31 01:29:56 +0900

    Add 클라이언트-서버 분리 작업_QR이미지 생성 부분

--- src/main/java/com/test/webtest/controller/ClientController.java
+++ src/main/java/com/test/webtest/controller/ClientController.java
@@ -1,11 +1,16 @@
 package com.test.webtest.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.test.webtest.domain.ImageVO;
 import com.test.webtest.domain.QrDTO;
 import com.test.webtest.service.QrService;
+import com.test.webtest.service.rest.RestInterface;
 import crypto.SecureQrCryptoArray;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import qr.generating.Generator;
+import retrofit2.Call;
+import retrofit2.Response;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
@@ -25,8 +32,9 @@ public class ClientController {
     //Service instance here
     @Autowired
     private QrService qrService;
+    private RestInterface restInterface;
     private SecureQrCryptoArray arr = new SecureQrCryptoArray();
-
+    private static final String CONTENT_TYPE = "application/json"; // header
 
     //Spring MVC here
     @GetMapping("/")
@@ -34,6 +42,12 @@ public class ClientController {
         return "home";
     }
 
+    /**
+     * 클라이언트 - 서버 동일 웹 어플리케이션에서 작동시 코드
+     * @param qrDTO
+     * @return
+     * @throws Exception
+     */
     @RequestMapping ("/request")
     public String connectToServer(QrDTO qrDTO) throws Exception{
         log.info(qrDTO.toString());
@@ -48,6 +62,25 @@ public class ClientController {
         log.info("이미지 렌더링 완료");
 
         return "redirect:";
+    }
+
+    /**
+     * 클라이언트 - 서버 분리시
+     * Retrofit 2 이용
+     */
+    @PostMapping("/requestQR")
+    public void request(QrDTO qrDTO) throws Exception{
+        // HTTP Body
+        String requestJson = new Gson().toJson(qrDTO, JsonObject.class);
+        String resourcePath = "generator";
+        // 요청
+        Call<ResponseBody> call = restInterface.generateQR(CONTENT_TYPE, resourcePath,requestJson);
+        Response<ResponseBody> response= call.execute();
+        byte[] result = response.body().bytes();
+
+        // 저장 - home.html 에서 이미지 보여줌
+        qrService.createQRImage(result, "C:\\TestQR\\qrImg\\Test3.png");
+
     }
 
 
--- src/test/java/com/test/webtest/WebtestApplicationTests.java
+++ src/test/java/com/test/webtest/WebtestApplicationTests.java
@@ -1,13 +1,26 @@
 package com.test.webtest;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.test.webtest.domain.QrDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @SpringBootTest
 class WebtestApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testParsingToJson(){
+		QrDTO qrDTO = new QrDTO("https://junho.com/", "https://test.com", 0, 250, 250);
+		String requestJson = new Gson().toJson(qrDTO, JsonObject.class);
+
 	}
 
 }
